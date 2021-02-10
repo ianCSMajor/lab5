b7d81aca2ddc56fffe24ff4a988f88ee55bf219d
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-02-10 15:23:41 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,87 @@
 #include "spreadsheet.hpp"
-
 #include "gtest/gtest.h"
 
+//for each test, we need a spreadsheet 
+//select row by calling set selection 
+TEST(AndTest, Works){
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_And* cell = new Select_And(new Select_Contains(&sheet, 
+                                     "Last", "Summers"), new Select_Contains(&sheet, "Age", "21"))
+    sheet.set_selection(cell);
+
+    EXPECT_EQ(cell->select(&sheet, 4));
+}
+TEST(OrTest, Works2){
+      Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_Or* cell1 = new Select_Or(new Select_Contains(&sheet, 
+                                     "Last", "Dole"), new Select_Contains(&sheet, "Age", "22"))
+
+    sheet.set_selection(cell1);
+
+    EXPECT_EQ(cell1->select(&sheet, 7))
+}
+TEST(ContainsTest, Substring){
+      Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_Contains* cell2 = new Select_Contains(new Select_Contains(&sheet, 
+                                     "First", "Car"));
+
+    sheet.set_selection(cell2);
+
+    EXPECT_EQ(cell1->select(&sheet, 2))
+}
+TEST(NotTest, Notstring){
+      Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_ Not* cell3 = new Select_Not(new Select_Contains(&sheet, 
+                                     "First", "Leslie"));
+
+    sheet.set_selection(cell3);
+
+    EXPECT_EQ(cell1->select(&sheet, 2))
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);

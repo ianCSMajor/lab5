db4646f004baaa80751ecc7f5c7fe8ae394e0d5e
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-02-10 17:20:34 -0800

    Update spreadsheet.cpp

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -40,8 +40,9 @@ int Spreadsheet::get_column_by_name(const std::string& name) const
             return i;
     return -1;
 }
-void Spreadsheet::print_selection(std::ostream& out)const{
-	if(select==NULL){
+void Spreadsheet::print_selection(std::ostream& out) const
+{
+    if(select==NULL){
 		for(int i=0;i<data.size();i++){
 			for(int j=0;j<column_names.size();j++){
 				out <<data[i][j]<< " ";

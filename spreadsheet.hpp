b1b0edd5dec055b53c7ee860738da489a020776e
ianCSMajor <57507406+ianCSMajor@users.noreply.github.com>
2021-02-10 17:32:37 -0800

    Update spreadsheet.hpp

--- spreadsheet.hpp
+++ spreadsheet.hpp
@@ -1,6 +1,3 @@
-#ifndef __SPREADSHEET_HPP__
-#define __SPREADSHEET_HPP__
-
 #include <string>
 #include <initializer_list>
 #include <vector>
@@ -31,11 +28,10 @@ public:
 
     // TODO: Implement print_selection.
     void print_selection(std::ostream& out) const;
+    
 
     void clear();
     void set_column_names(const std::vector<std::string>& names);
     void add_row(const std::vector<std::string>& row_data);
     int get_column_by_name(const std::string& name) const;
 };
-
-#endif //__SPREADSHEET_HPP__
